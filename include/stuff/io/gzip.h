bd63216b6ee6f83b878bdf5a10805b9c80f86456
Tony Walker <tonywalker1@icloud.com>
2019-09-06 12:53:12 -0400

    Start gzip support.

---
+++ include/stuff/io/gzip.h
@@ -0,0 +1,42 @@
+// Copyright (C) 2019  Tony Walker
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef STUFF_GZIP_H
+#define STUFF_GZIP_H
+
+#include <stuff/core/exception.h>
+#include <stuff/io/filesystem.h>
+#include <zlib.h>
+
+namespace stuff {
+
+    STUFF_DEFINE_EXCEPTION(gzip_error, generic_error);
+
+    //
+    class gzip_file {
+    public:
+
+        gzip_file(fs::path path, const char* mode = "r");
+        ~gzip_file();
+
+    private:
+
+        gzFile m_handle;
+
+    }; // class gzip_file
+
+} // namespace stuff
+
+#endif // STUFF_GZIP_H
